8cd549fe174108c242551826650a98ca6064bb28
Levente <122294490+Levi-24@users.noreply.github.com>
2024-01-12 11:20:33 +0100

    Init

---
+++ Iroda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Irodahaz
+{
+    internal class Iroda
+    {
+        public string cegNev { get; set; }
+        public int bekoltozesEve { get; set; }
+        public List<int> irodaLetszam { get; set; }
+
+        public Iroda(string s)
+        {
+            var pieces = s.Split(" ");
+            this.cegNev = pieces[0];
+            this.bekoltozesEve = int.Parse(pieces[1]);
+            irodaLetszam = new List<int>();
+            for (int i = 2; i < pieces.Length; i++) { this.irodaLetszam.Add(int.Parse(pieces[i])); }
+        }
+
+        public override string ToString()
+        {
+            string irodistak = "";
+            for (int i = 0; i < irodaLetszam.Count; i++) { irodistak += irodaLetszam[i] + " "; }
+            return $"{this.cegNev};".PadRight(18) + $"{this.bekoltozesEve};".PadRight(10) + $"{irodistak}";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Irodahaz;
+using System.Text;
+
+List<Iroda>Irodak = new List<Iroda>();
+using StreamReader sr = new StreamReader("../../../src/irodahaz.txt", Encoding.UTF8);
+while (!sr.EndOfStream) Irodak.Add(new Iroda(sr.ReadLine()));
+
+Console.WriteLine("Az irodaház adatai:");
+Console.WriteLine("\tNév\t\tÉv\tIrodisták száma");
+
+for (int i = 0; i < Irodak.Count; i++)
+{
+    Console.Write(i+1 + ". ");
+    Console.WriteLine(Irodak[i]);
+}
+
+Console.WriteLine("8.Feladat:");
+var max = Irodak.Max(x => x.irodaLetszam.Count());
+Console.WriteLine(Irodak[max]);
+
+Console.WriteLine("9.Feladat:");
+var kilenc = Irodak.Where(x => x.irodaLetszam.Contains(9));
+foreach (var item in kilenc) Console.WriteLine(item.ToString());
+
+Console.WriteLine("10.Feladat:");
+var ot = Irodak.Where(x => x.irodaLetszam.Contains(5));
+foreach (var item in ot) Console.WriteLine(item.ToString());
